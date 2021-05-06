2b779523b18e6c63d2d1cb9d2dbabcf18e48f012
HGlab <54232526+HGlab01@users.noreply.github.com>
2021-05-06 15:43:06 +0200

    add attribute enabledFeatures

--- main.js
+++ main.js
@@ -816,13 +816,11 @@ class Tado extends utils.Adapter {
 					break;
 
 				case ('enabledFeatures'):
-					this.create_state(HomeId + '._info. ' + i, i, this.Home_data[i]);
+					this.create_state(HomeId + '._info. ' + i, i, JSON.stringify(this.Home_data[i]));
 					break;
-					
+
 				default:
 					this.log.warn(`Send this info to developer !!! { Unhandable information found in DoHome: ${JSON.stringify(i)} with value: ${JSON.stringify(this.Home_data[i])}`);
-
-
 			}
 			// }
 		}
